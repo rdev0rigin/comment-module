d66c48c8c8ddf491dc227d8e13e1247f8919c597
Raven <rdev1163@outlook.com>
2017-06-28 11:15:49 -0700

    added a demo-module and built a recursive component for replies, also added helper services

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # RDev Comment Module
 
 An Angular Module made to inject a comment window.
+
+## NPM Install
+```shell
+npm i --save ngrdev-comment-module
+```
--- rdev-comment.module.ts
+++ rdev-comment.module.ts
@@ -1,9 +1,6 @@
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MainComponent} from './main/main.component';
-import {HomeComponent} from './components/home.component';
 import {CommentsComponent} from './components/comments.component';
 import {CommentComponent} from './components/comment.component';
 import {CommentsService} from './services/comments.service';
@@ -31,8 +28,6 @@ const COMPONENTS = [
 	schemas: [
 		CUSTOM_ELEMENTS_SCHEMA
 	],
-	bootstrap: [ MainComponent ],
-	entryComponents: [MainComponent]
 })
 
 export class RDevCommentModule {}
